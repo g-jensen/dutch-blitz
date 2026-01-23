8cc195b49e9482b1c6940f194f10e01e267395e7
greg <greg.micah.jensen@gmail.com>
2026-01-22 23:24:05 -0500

    update spacing

--- internal/game/game.go
+++ internal/game/game.go
@@ -11,18 +11,25 @@ type Card struct {
 
 type Game interface {
 	Players() []Player
+
 	Hand(player Player) []Card
 	SetHand(player Player, newHand []Card)
+
 	WoodPile(player Player) []Card
 	SetWoodPile(player Player, newWoodPile []Card)
+
 	BlitzPile(player Player) []Card
 	SetBlitzPile(player Player, newBlitzPile []Card)
+
 	PostPiles(player Player) [][]Card
 	SetPostPiles(player Player, newPostPiles [][]Card)
+
 	DutchPiles() [][]Card
 	SetDutchPiles(newDutchPiles [][]Card)
+
 	HasPlayedThisReset(player Player) bool
 	SetHasPlayedThisReset(player Player, value bool)
+
 	IsCyclable(player Player) bool
 	SetCyclable(player Player, value bool)
 }
